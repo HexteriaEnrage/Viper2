3d3e6af5fd86ec44d65ebe6a17af3020f4e92b0f
BuildTools <silence@lyreoss.com>
2021-03-27 04:20:35 -0700

    Per-map and centered cards
    -Cards now generated per map using genCard("mapname")
    -Cards are now centered on screen

--- cardgen.js
+++ cardgen.js
@@ -1,8 +1,9 @@
 var cardJSON = {
-    "cards": [
+    "ascent": [
         {
             "img": "images/placeholder/unknown.png",
-            "desc": "Somewhere to somewhere else"
+            "desc": "Somewhere to somewhere else",
+            "tags": ["Orb","A Site","A Long"]
         },
         {
             "img": "images/placeholder/unknown.png",
@@ -36,25 +37,27 @@ var cardJSON = {
             "img": "images/placeholder/unknown.png",
             "desc": "Somewhere to somewhere else"
         },
-        
+
         {
             "img": "images/placeholder/unknown.png",
             "desc": "This is a different card."
         }
     ]
 }
-function genCard() {
-    for (i = 0; i < cardJSON.cards.length; i++) {
+function genCard(map) {
+    var container = document.getElementById("cardContainer");
+    var pArray = cardJSON[map];
+    for (i = 0; i < pArray.length; i++) {
         var pDiv = document.createElement('div');
         pDiv.setAttribute('class', 'gallery');
         var iA = document.createElement('a');
-        iA.innerHTML = `<img src="${cardJSON.cards[i].img}" alt="" >`;
-        iA.setAttribute('href', cardJSON.cards[i].img);
+        iA.innerHTML = `<img src="${pArray[i].img}" alt="" >`;
+        iA.setAttribute('href', pArray[i].img);
         pDiv.appendChild(iA);
         var descDiv = document.createElement('div');
         descDiv.setAttribute('class', 'card-desc');
-        descDiv.innerHTML = `<p>${cardJSON.cards[i].desc}</p><div>Learn more...</div>`
+        descDiv.innerHTML = `<p>${pArray[i].desc}</p><div>Learn more...</div>`
         pDiv.appendChild(descDiv);
-        document.body.appendChild(pDiv);
+        container.appendChild(pDiv);
     }
 }
