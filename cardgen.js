aff4670022ea8f78dfc734831be683f65cad35b9
BuildTools <silence@lyreoss.com>
2021-03-27 05:21:05 -0700

    Card tags/symbols
    -Cards now have tags
    -Cards now have symbols indicating the ability

--- cardgen.js
+++ cardgen.js
@@ -3,11 +3,14 @@ var cardJSON = {
         {
             "img": "images/placeholder/unknown.png",
             "desc": "Somewhere to somewhere else",
-            "tags": ["Orb","A Site","A Long"]
+            "symbol": "C",
+            "tags": ["Orb","A Site","A Long","A Long","A Long","A Long","A Long"]
         },
         {
             "img": "images/placeholder/unknown.png",
-            "desc": "Somewhere to somewhere else"
+            "desc": "Somewhere to somewhere else",
+            "symbol": "X",
+            "tags": ["Orb","A Site","A Long","A Long","A Long","A Long","A Long"]
         },
         {
             "img": "images/placeholder/unknown.png",
@@ -54,9 +57,25 @@ function genCard(map) {
         iA.innerHTML = `<img src="${pArray[i].img}" alt="" >`;
         iA.setAttribute('href', pArray[i].img);
         pDiv.appendChild(iA);
+        var symbolSprite = document.createElement('img');
+        symbolSprite.setAttribute('src', `images/TX_Viper_${pArray[i].symbol}.png`);
+        symbolSprite.setAttribute('class', 'card-symbol card-c');
+        pDiv.appendChild(symbolSprite);
         var descDiv = document.createElement('div');
         descDiv.setAttribute('class', 'card-desc');
-        descDiv.innerHTML = `<p>${pArray[i].desc}</p><div>Learn more...</div>`
+        descDiv.innerHTML = `<p>${pArray[i].desc}</p>`
+        if(pArray[i].tags != null)
+        {
+            for(b = 0; b < pArray[i].tags.length; b++)
+            {
+                var tagDiv = document.createElement('div');
+                tagDiv.setAttribute('class', 'tag');
+                var tagText = document.createElement('p');
+                tagText.textContent = pArray[i].tags[b];
+                tagDiv.appendChild(tagText);
+                descDiv.appendChild(tagDiv);
+            }
+        }
         pDiv.appendChild(descDiv);
         container.appendChild(pDiv);
     }
