7a57516c60d9007b6272edcd28de510ebebb504d
BuildTools <silence@lyreoss.com>
2021-03-27 06:51:41 -0700

    Sorting implemented
    -Cards can now be sorted by clicking on the card tag

--- cardgen.js
+++ cardgen.js
@@ -4,46 +4,51 @@ var cardJSON = {
             "img": "images/placeholder/unknown.png",
             "desc": "Somewhere to somewhere else",
             "symbol": "C",
-            "tags": ["Orb","A Site","A Long","A Long","A Long","A Long","A Long","A Site","A Long","A Long","A Long","A Long","A Long","A Site","A Long","A Long","A Long","A Long","A Long"]
+            "tags": ["Orb", "A Site", "A Long", "A Long", "A Long", "A Long", "A Long", "A Site", "A Long", "A Long", "A Long", "A Long", "A Long", "A Site", "A Long", "A Long", "A Long", "A Long", "A Long"]
         },
         {
             "img": "images/placeholder/unknown1.png",
             "desc": "Bind A Molly",
             "symbol": "C",
-            "tags": ["Snakebite","A Site","A Bath"]
+            "tags": ["Snakebite", "A Site", "A Bath"]
         },
         {
-            "img": "images/placeholder/unknown.png",
-            "desc": "Somewhere to somewhere else"
+            "img": "images/placeholder/unknown2.png",
+            "desc": "A Short Retake Oneway",
+            "symbol": "Q",
+            "tags": ["Orb", "Oneway", "Retake", "A Site", "A Short"]
         },
         {
-            "img": "images/placeholder/unknown.png",
-            "desc": "Somewhere to somewhere else"
+            "img": "images/placeholder/unknown1.png",
+            "desc": "Bind A Molly",
+            "symbol": "C",
+            "tags": ["Orb", "B Site", "A Bath"]
         },
         {
-            "img": "images/placeholder/unknown.png",
-            "desc": "Somewhere to somewhere else"
+            "img": "images/placeholder/unknown1.png",
+            "desc": "Bind A Molly",
+            "symbol": "C",
+            "tags": ["Snakebite", "B Site", "A Bath"]
+        }
+    ],
+    "filter": [
+        {
+            "img": "images/placeholder/unknown1.png",
+            "desc": "Bind A Molly",
+            "symbol": "C",
+            "tags": ["Snakebite", "A Site", "A Bath"]
         },
         {
-            "img": "images/placeholder/unknown.png",
-            "desc": "Somewhere to somewhere else"
+            "img": "images/placeholder/unknown1.png",
+            "desc": "Bind A Molly",
+            "symbol": "C",
+            "tags": ["Snakebite", "A Site", "A Bath"]
         },
         {
-            "img": "images/placeholder/unknown.png",
-            "desc": "Somewhere to somewhere else"
-        },
-        {
-            "img": "images/placeholder/unknown.png",
-            "desc": "Somewhere to somewhere else"
-        },
-        {
-            "img": "images/placeholder/unknown.png",
-            "desc": "Somewhere to somewhere else"
-        },
-
-        {
-            "img": "images/placeholder/unknown.png",
-            "desc": "This is a different card."
+            "img": "images/placeholder/unknown1.png",
+            "desc": "Bind A Molly",
+            "symbol": "C",
+            "tags": ["Snakebite", "A Site", "A Bath"]
         }
     ]
 }
@@ -64,22 +69,46 @@ function genCard(map) {
         var descDiv = document.createElement('div');
         descDiv.setAttribute('class', 'card-desc');
         descDiv.innerHTML = `<p>${pArray[i].desc}</p>`
-        if(pArray[i].tags != null)
-        {
-            for(b = 0; b < pArray[i].tags.length; b++)
-            {
+        if (pArray[i].tags != null) {
+            for (b = 0; b < pArray[i].tags.length; b++) {
                 var tagDiv = document.createElement('div');
                 tagDiv.setAttribute('class', 'tag');
+                tagDiv.setAttribute('onclick', `filter('${pArray[i].tags[b]}', '${map}')`);
                 var tagText = document.createElement('p');
                 tagText.textContent = pArray[i].tags[b];
                 tagDiv.appendChild(tagText);
                 descDiv.appendChild(tagDiv);
             }
         }
-        var lMore = document.createElement('button');
-        lMore.innerHTML = 'Learn more...';
         pDiv.appendChild(descDiv);
-        pDiv.appendChild(lMore);
         container.appendChild(pDiv);
     }
+}
+var lastSorted;
+function filter(filterTag, map) {
+    if(map == 'filter')
+    {
+        map = lastSorted;
+    } else {
+        lastSorted = map;
+    }
+    var pDiv = document.getElementById("cardContainer");
+    pDiv.innerHTML = '';
+
+    var filterArray = [];
+    for (u = 0; u < cardJSON[map].length; u++) {
+
+        console.log(cardJSON[map][u].tags + '\n' + '================');
+        for (z = 0; z < cardJSON[map][u].tags.length; z++) {
+
+            if (filterTag == cardJSON[map][u].tags[z]) {
+                filterArray.push(cardJSON[map][u]);
+                console.log(cardJSON[map][u].tags);
+                break;
+            }
+        }
+    }
+    cardJSON.filter = filterArray;
+    console.log(cardJSON.filter);
+    genCard("filter");
 }
